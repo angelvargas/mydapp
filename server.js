19e6604de0e7759a6145e81e7a0d0497163ae0af
Angel Vargas <angelvargaspool@gmail.con>
2019-08-30 11:40:52 -0500

    test web3js with kaleido

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = 3001;
 //
 let USER = "";
 let PASS = "";
-let RPC_ENDPOINT = ""; // Remove the leading https://
+let RPC_ENDPOINT = "";
 
 // HTTP Provider Example
 // NOTE: The HTTP Provider is deprecated, as it won't work for subscriptions.
@@ -45,8 +45,7 @@ createWallet(result => {
     //console.log("Private Key is:", result.privateKey);
 })
 //web3.eth.personal.sign("Hello world", "0xA9E9B03299fF4F59cEdB75791A435C75fD4D2D39", "!@superpassword").then(console.log);
-web3.eth.personal.unlockAccount("0xA9E9B03299fF4F59cEdB75791A435C75fD4D2D39", "!@superpassword", 600)
-.then(console.log('Account unlocked!'));
+web3.eth.personal.unlockAccount("0xA9E9B03299fF4F59cEdB75791A435C75fD4D2D39", "!@superpassword", 600).then(console.log('Account unlocked!'));
 //web3.eth.accounts.wallet;
 /*
 web3.eth.getBlock("latest").then((latestBlock) => {
@@ -68,30 +67,3 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
-
-/*
-const Web3 = require('web3');
-
-let USER = "u0mjgm2y4w";
-let PASS = "Djdo9n5iCIMY1s3AbvN77MFXbDn800idndJmvpSSwVI";
-let RPC_ENDPOINT = "u0t1wfxa79-u0i4zhl18v-rpc.us0-aws.kaleido.io"; // Remove the leading https://
-
-// HTTP Provider Example
-// NOTE: The HTTP Provider is deprecated, as it won't work for subscriptions.
-// See: https://web3js.readthedocs.io/en/1.0/web3.html#providers
-
-let nodeUrl = "https://" + USER + ":" + PASS + "@" + RPC_ENDPOINT;
-
-let provider = new Web3.providers.HttpProvider(nodeUrl);
-let web3 = new Web3(provider);
-
-// Now you can call web3 functions, so we'll just test the connection by getting the latest block in the chain.
-
-web3.eth.getBlock("latest").then((latestBlock) => {
-    console.log("Latest Block Via HTTP Provider: ")
-    console.log(latestBlock);
-    // Stop the program once this has finished
-    process.exit();
-});
-
-*/
